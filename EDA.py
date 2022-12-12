b136969ee6180f1dd7bcd45a510837e004f23fc0
rosavrio <rolovesduff@gmail.com>
2022-12-12 13:58:05 +0100

    EDA commit ticket 2 modificacion"
    git status
    
    git add EDA.py

--- EDA.py
+++ EDA.py
@@ -11,3 +11,21 @@ def read_dataset():
    global data
 my_dataset = './data/iris.csv'
 data = explore_data(my_dataset)
+
+def select_column():
+  # Select A Column
+  col_option = st.selectbox("Select Column", ("SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"))
+  if col_option == "SepalLengthCm":
+    st.write(data["SepalLengthCm"])
+    #st.write(type(data["SepalLengthCm"]))
+  elif col_option == "SepalWidthCm":
+    st.write(data[["SepalWidthCm"]])
+    #st.write(type(data[["SepalWidthCm"]]))
+  elif col_option == "PetalLengthCm":
+    st.write(data[["PetalLengthCm"]])
+  elif col_option == "PetalWidthCm":
+    st.write(data[["PetalWidthCm"]])
+  elif col_option == "Species":
+    st.write(data[["Species"]])
+  else:
+    st.write("Select a Column")
